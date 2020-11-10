3a4fa66d868a46c434372298c9fdfd51a656219b
Student   CLARK Dan <05365@farnborough.ac.uk>
2020-11-10 09:49:37 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String Greeting = "Hello World!";
+        System.out.println(Greeting);
+        System.out.println(Greeting.length())
+
+
+    }
+}
