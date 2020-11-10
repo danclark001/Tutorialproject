08fb951b08cb0c355837310d9a17507d1e095675
05365 <05365@farnborough.ac.uk>
2020-11-10 09:53:30 +0000

    here some

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,8 @@ public class Main {
     public static void main(String[] args) {
         String Greeting = "Hello World!";
         System.out.println(Greeting);
-        System.out.println(Greeting.length())
+        System.out.println(Greeting.length());
+        System.out.println(Greeting + "modda focker");
 
 
     }
